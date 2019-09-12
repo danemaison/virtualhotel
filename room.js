cb7e7b7cb17e8185f83e52b77a1a763a93e97b91
Mitchell O'Hair <mitchellohair@gmail.com>
2019-09-12 15:23:41 -0700

    Constructed base Room class

---
+++ room.js
@@ -0,0 +1,33 @@
+class Room {
+  constructor (number, checkout) {
+    this.roomNumber = number;
+    this.isVacant = true;
+    this.customer = null;
+
+    this.checkout = checkout;
+    this.removeCustomer = this.removeCustomer.bind(this);
+  }
+
+  addCustomer (customer) {
+    this.isVacant = false;
+    this.customer = customer;
+    this.customer.roomNumber = this.roomNumber;
+  }
+
+  removeCustomer (checkout) {
+    this.isVacant = true;
+    this.customer.roomNumber = null;
+
+    if (checkout) {
+      console.log('Customer is checking out');
+      this.checkout(this.customer);
+    } else {
+      console.log('Customer leaving room');
+    }
+    this.customer = null;
+  }
+
+  toString () {
+    return `Room #${this.roomNumber} is ${(this.isVacant) ? 'vacant' : 'occupied'}`;
+  }
+}
